d0b83e1509230906767d7f0d5a2321d530c473bc
Viktor <imjustsnoopy@gmail.com>
2021-03-15 15:26:32 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 module.exports = function towelSort (matrix) {
     for (i in matrix) {
-        if (i % 2) {
-            matrix[i].reverse()
-        }
+        if (i % 2) { matrix[i].reverse() }
     }
     return matrix === undefined ? [] :  matrix.flat()
 }
